f5a18edd7ebecbc4c30b7a2038f1ae5c99455dc6
Josh Steiner <josh@jsteiner.me>
2015-04-13 10:28:26 -0400

    Comment out settings producing noisy output
    These settings are useful for specific things, but noisy otherwise. We
    don't need them while we're learning.

--- example_app/spec/spec_helper.rb
+++ example_app/spec/spec_helper.rb
@@ -42,8 +42,8 @@ RSpec.configure do |config|
   # to individual examples or groups you care about by tagging them with
   # `:focus` metadata. When nothing is tagged with `:focus`, all examples
   # get run.
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
+  # config.filter_run :focus
+  # config.run_all_when_everything_filtered = true
 
   # Limits the available syntax to the non-monkey patched syntax that is recommended.
   # For more details, see:
@@ -65,7 +65,7 @@ RSpec.configure do |config|
   # Print the 10 slowest examples and example groups at the
   # end of the spec run, to help surface which specs are running
   # particularly slow.
-  config.profile_examples = 10
+  # config.profile_examples = 10
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
