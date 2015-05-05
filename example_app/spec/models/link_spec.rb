09d238dbab61242eed385e61a966a4d26cdb6af7
Josh Steiner <josh@jsteiner.me>
2015-05-05 10:58:19 -0400

    expect(subject) -> is_expected

--- example_app/spec/models/link_spec.rb
+++ example_app/spec/models/link_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Link, "validations" do
-  it { expect(subject).to validate_presence_of(:title) }
-  it { expect(subject).to validate_presence_of(:url) }
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:url) }
 end
 
 RSpec.describe Link, "#upvote" do
