bbac4fd4e524408311538a00a081f63b96fd17e3
Joël Quenneville <joelq@thoughtbot.com>
2015-09-15 14:17:54 -0400

    Test controller in isolation via stubbing
    Previously the controller test could fail if the validations on `Link`
    changed. Now this test purely checks to see that we re-render the form
    when the link is invalid (regardless of what it takes for a link to be
    invalid)

--- example_app/spec/controllers/links_controller_spec.rb
+++ example_app/spec/controllers/links_controller_spec.rb
@@ -3,7 +3,10 @@ require "rails_helper"
 RSpec.describe LinksController, "#create" do
   context "when the link is invalid" do
     it "re-renders the form" do
-      post :create, link: attributes_for(:link, :invalid)
+      invalid_link = double(save: false)
+      allow(Link).to receive(:new).and_return(invalid_link)
+
+      post :create, link: { attribute: "value" }
 
       expect(response).to render_template :new
     end
