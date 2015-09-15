db8110e40a4cc40a3c10060c33db0645e4455d48
Joël Quenneville <joelq@thoughtbot.com>
2015-09-15 16:01:37 -0400

    Rewrite the link_controller spec with spies

--- example_app/spec/controllers/links_controller_spec.rb
+++ example_app/spec/controllers/links_controller_spec.rb
@@ -16,10 +16,11 @@ RSpec.describe LinksController, "#create" do
     it "sends an email to the moderators" do
       valid_link = double(save: true)
       allow(Link).to receive(:new).and_return(valid_link)
-
-      expect(LinkMailer).to receive(:new_link).with(valid_link)
+      allow(LinkMailer).to receive(:new_link)
 
       post :create, link: { attribute: "value" }
+
+      expect(LinkMailer).to have_received(:new_link).with(valid_link)
     end
   end
 end
